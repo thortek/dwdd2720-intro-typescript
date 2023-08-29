a5170d3646e02d8cac1a187d341908ed6c1db6c6
Thor Anderson <ta.anderson@gmail.com>
2023-08-29 15:14:57 -0600

    Basic tsc and vite config working

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+## Web Languages 1 - Introduction to TypeScript
+
+This is where the code is posted for the DWDD-2720 class during the Fall 2023 semester
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+
+const root = resolve(__dirname, "src")
+const outDir = resolve(__dirname, "dist")
+
+export default defineConfig({
+    root,
+    build: {
+        rollupOptions: {
+            outDir,
+            input: {
+                main: resolve(root, "index.html")
+            }
+        }
+    }
+})
