c22ad8e7e4fbd0cdcfc6489a3911e27da9b8c153
Thor Anderson <ta.anderson@gmail.com>
2023-09-05 15:32:04 -0600

    Added nav to show basic types examples

---
+++ dist/assets/main-0d7901b1.js
@@ -0,0 +1,1 @@
+import"./index-75b77730.js";console.log("Hello World!");const t=document.querySelector("#message");t.textContent=n(5,10);function n(e,o){return(e+o).toString()}
---
+++ src/basic-types.ts
@@ -0,0 +1,29 @@
+
+//console.log(foo)
+
+let foo = 123; // this is declaration with an initial assignment and the data type is inferred
+
+const bar: string = "456"; // this is a declaration with an initial assignment and the data type is specified using a type annotation
+
+// var foo = 123; // this is a declaration with an assignment
+
+// Data types added by TypeScript are:
+// - boolean
+// - number
+// - string
+// - array
+// - tuple
+// - enum
+// - any
+// - void
+// - null
+// - undefined
+// - never
+// - object
+
+
+
+//foo = "456"; // this is a re-assignment
+//foo = true; // and another
+//foo = {}; // and another
+//foo = []; // and another
