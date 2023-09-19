f35c7667cd2512f4a497c414c80ce82c851ce0d0
Thor Anderson <ta.anderson@gmail.com>
2023-09-19 15:36:02 -0600

    Used interface for code examples

--- src/basic-types.ts
+++ src/basic-types.ts
@@ -15,7 +15,15 @@ let myNumber: number = 42;
 // String
 let myString: string = "Hello TypeScript!";
 // myString is a variable annotated with the string data type and assigned the value "Hello TypeScript!"
+`
+    }
+}
 
+export function arrayTypesExample() {
+    return {
+        title: `Array Types`,
+        explanation: `TypeScript has a few different types for arrays, including array, tuple, and enum`,
+        code: `
 // Array
 let myArray: number[] = [1, 2, 3, 4];
 // myArray is a variable annotated with the array data type and assigned the value [1, 2, 3, 4]
@@ -34,7 +42,15 @@ enum Color {
     MidnightBlue,
 }
 // Color is a variable annotated with the enum data type and assigned the value {Red: 0, Silver: 1, White: 2, Black: 3, Bahama_Green: 4, Midnight_Blue: 5}
+`
+    }
+}
 
+export function weirdTypesExample() {
+    return {
+        title: `Weird Types`,
+        explanation: `TypeScript has a few "weird" types, including any, void, null, undefined, and never.`,
+        code: `
 // Any
 let myVariable: any = "Hello World";
 myVariable = 42; 
@@ -56,13 +72,30 @@ function neverReturns(): never {
     throw new Error("An error!");
 } // never is a data type that represents the type of values that never occur
 // an example of a function that returns never is a function that always throws an error
+`
+    }
+}
 
+export function objectTypesExample() {
+    return {
+        title: `Object Types`,
+        explanation: `TypeScript has a few different types for objects, including object and interface`,
+        code: `
 // Object
 let myObject: object = { foo: "bar" };
 // myObject is a variable annotated with the object data type and assigned the value { foo: "bar" }
 // Using the 'object' datatype should be done with caution and in specific use cases.  It can be useful when dealing with situations where you need to work with dynamic or unknown data structures, such as when handling data from external sources or dealing with generic types.
 // However, using the 'object' datatype too liberally can lead to coe that is harder to reason about, maintain, and debug. It is best to use more specific and precise types whenever possible.
 
+// Interface
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+// The interface definition defines a TypeScript interface called "Person". This interface has three properties: "firstName"
+//which is a string, "lastName" which is also a string, and "age" which is a number. 
+//This interface can be used to define objects that must have these three properties and their respective data types.}
 `
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,44 @@
-import { basicTypesExample, functionParamsExample } from "./basic-types"
+import { basicTypesExample, arrayTypesExample, weirdTypesExample, objectTypesExample } from "./basic-types"
 import hljs from "highlight.js/lib/core"
 import typescript from "highlight.js/lib/languages/typescript"
 hljs.registerLanguage("typescript", typescript)
 
 //const messageParagraph = document.querySelector("#message")
-const basicTypesButton = document.querySelector("#basicTypesButton")
+document.querySelector("#basicTypesButton")?.addEventListener("click", () => loadExample("basic"))
+document.querySelector("#arrayTypesButton")?.addEventListener("click", () => loadExample("array"))
+document.querySelector("#weirdTypesButton")?.addEventListener("click", () => loadExample("weird"))
+document.querySelector("#objectTypesButton")?.addEventListener("click", () => loadExample("object"))
 
 //const cardBody = document.querySelector("#cardBody")
 const cardTitle = document.querySelector("#cardTitle")
 const cardExplanation = document.querySelector("#cardExplanation")
 const cardCode = document.querySelector("#cardCode")
 
-basicTypesButton?.addEventListener("click", () => {
-    //messageParagraph!.textContent = "Hello Basic Types!"
-    loadExample()
-})
+interface Example {
+    title: string;
+    explanation: string;
+    code: string;
+}
 
-//console.log(functionParamsExample())
-
-function loadExample() {
-    const example = basicTypesExample()
-    cardTitle!.textContent = example.title
-    cardExplanation!.textContent = example.explanation
-    cardCode!.innerHTML = example.code
+function loadExample(exampleType: string = "basic") {
+    let chosenExampleType: Example
+    switch (exampleType) {
+        case "array":
+            chosenExampleType = arrayTypesExample()
+            break
+        case "weird":
+            chosenExampleType = weirdTypesExample()
+            break
+        case "object":
+            chosenExampleType = objectTypesExample()
+            break
+        default:
+           chosenExampleType = basicTypesExample()
+    }
+    
+    cardTitle!.textContent = chosenExampleType.title
+    cardExplanation!.textContent = chosenExampleType.explanation
+    cardCode!.innerHTML = chosenExampleType.code
     hljs.highlightAll()
 }
 
