a73b0e3abcd3f671267e9f95099f715a41340bb4
Thor Anderson <ta.anderson@gmail.com>
2023-09-26 15:36:06 -0600

    Added new classes route

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
             input: {
                 main: resolve(root, "index.html"),
                 functionTypes: resolve(root, 'functionTypes', "index.html"),
+                classes: resolve(root, 'classes', "index.html"),
             }
         }
     }
